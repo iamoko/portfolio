d72f2e5276343b30479d48295c5b287f4f018dfe
Amoko Ivan <amokoivan@gmail.com>
2023-08-09 13:49:24 +0300

    Profile design animation

--- src/layout/profile.js
+++ src/layout/profile.js
@@ -1,38 +1,30 @@
 import React from "react";
 import Carousel from "../components/carousel/carousel";
+import _ from "lodash";
 
 function Profile() {
+  const images = [
+    "https://www.w3schools.com/howto/img_snow_wide.jpg",
+    "https://www.w3schools.com/howto/img_woods_wide.jpg",
+    "https://www.w3schools.com/howto/img_lights_wide.jpg",
+  ];
   return (
     <div
-      className="z-10 w-full overflow-hidden border-black rounded-md main"
+      className="relative z-10 w-full overflow-hidden border-black rounded-md main"
       style={{ boxShadow: "rgba(0, 0, 0, 0.15) 2.4px 2.4px 3.2px" }}
     >
-      <div className="h-1/2">
+      <div className="h-[80%]">
         <Carousel>
-          <div>
-            <img
-              src="https://www.w3schools.com/howto/img_snow_wide.jpg"
-              alt="img1"
-            />
-          </div>
-          <div>
-            <img
-              src="https://www.w3schools.com/howto/img_woods_wide.jpg"
-              alt="img2"
-            />
-          </div>
-          <div>
-            <img
-              src="https://www.w3schools.com/howto/img_lights_wide.jpg"
-              alt="img3"
-            />
-          </div>
+          {_.map(images, (img, i) => (
+            <div>
+              <img key={i} src={img} alt="img1" className="object-cover" />
+            </div>
+          ))}
         </Carousel>
-        {/* <img
-          src={"https://clipart-library.com/images/kiMKppb4T.jpg"}
-          alt="cover"
-          className="object-cover w-full h-full"
-        /> */}
+      </div>
+
+      <div className="absolute top-[40%] ml-[-50%] rounded-t-full  h-[500px] main w-[200%] ">
+        <p>dfghj</p>
       </div>
     </div>
   );
